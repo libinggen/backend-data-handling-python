39b834cbdf0f57fd487977f242fab6e56e3ae878
libinggen <libinggengit@gmail.com>
2023-12-24 02:15:11 +0900

    ok

--- djangoproject/djangoapp/models.py
+++ djangoproject/djangoapp/models.py
@@ -3,7 +3,7 @@ import uuid
 
 
 class User(models.Model):
-    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     name = models.CharField(max_length=50, unique=True)
     email = models.EmailField(max_length=250, unique=True)
     password = models.CharField(max_length=14, null=True, blank=True)
